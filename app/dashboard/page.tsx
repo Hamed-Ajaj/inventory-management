7ac852c07340b429e4cc763fc545d1be32b9a799
hamed ajaj <hamedsgaming@gmail.com>
2025-10-16 00:15:29 +0300

    better auth init, with prisma

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+
+async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (!session) {
+    return <div>Not authenticated</div>
+  }
+  return (
+    <div>
+      <h1>Welcome {session.user.name}</h1>
+    </div>
+  )
+}
+
+export default DashboardPage;
